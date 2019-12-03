8eeff0b6a1bba333d0d8db3a1723f3b1583e5346
Taha <taha.nagy06@gmail.com>
2019-12-02 22:06:07 -0800

    Initial Commit

---
+++ NotaApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotaApp
+//
+//  Created by Taha on 12/2/19.
+//  Copyright © 2019 Taha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
